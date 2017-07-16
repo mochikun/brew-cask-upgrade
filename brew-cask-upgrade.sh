623089ee604b1034e36dabca194ff504be2ee781
mochikun <flogedge+github@gmail.com>
2017-07-16 09:04:59 +0900

    fixed: removed head broken pipe error message

--- brew-cask-upgrade.sh
+++ brew-cask-upgrade.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/fish
 
 for c in (brew cask list)
-	set ver (brew cask info $c | head -n 1 | cut -d " " -f2)
+	set ver (brew cask info $c 2>/dev/null | head -n 1 | cut -d " " -f2)
 	if [ ! -e /usr/local/Caskroom/$c/$ver ]
 		brew cask install --force $c
 	end
